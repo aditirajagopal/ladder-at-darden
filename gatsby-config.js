5879569662d09fd0efdbdb2e3bd18ad7a582a104
Aditi Rajagopal <aditi.rajagopal92@gmail.com>
2020-06-11 21:19:41 -0400

    fixing metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Ladder',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Ladder is a network of women who want to maximize their ROI in consulting - whether that\'s to get on the right projects, get the next promotion, exit into industry or rise to partner.',
   },
   plugins: [
     'gatsby-plugin-sharp',
